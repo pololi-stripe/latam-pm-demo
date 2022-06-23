6ef067d00eb893ca993ba668abfb2c5a57a1df7a
Polo Li <pololi@stripe.com>
2022-06-23 11:51:05 -0700

    pl - setup nodemon for hot reload

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server! sdfjdklafjd;afjdlks;a" });
 });
 
 // All other GET requests not handled before will return our React app
