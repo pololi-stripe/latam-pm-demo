802524327d1991612a5fb131c829801a4fb0c55d
Polo Li <pololi@stripe.com>
2022-06-24 15:27:22 -0700

    pl - make req.protocol to have https
    https://stackoverflow.com/questions/40459511/in-express-js-req-protocol-is-not-picking-up-https-for-my-secure-link-it-alwa

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.enable('trust proxy');
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
